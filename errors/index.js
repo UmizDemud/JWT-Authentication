d873e7a91540f90a80f5f0bf7dc6b890a8eb8345
Demut <uddarendeli1@gmail.com>
2022-10-18 15:08:14 +0300

    Code of the completed project docking

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require("./custom-error.js");
+const BadRequest = require("./bad-request.js");
+const Unauthenticated = require("./unauthenticated.js");
+
+module.exports = {
+	CustomAPIError,
+	BadRequest,
+	Unauthenticated,
+};
